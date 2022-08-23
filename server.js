93520fd57d6007fcae3c84f02ade7e4eddf36fa5
ASWANTH KRISHNA M <chithukrishna.m@gmail.com>
2022-08-24 01:13:19 +0530

    ver 1.1

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 
 const app=require('./app')
-const dotenv=require('dotenv')
+//const dotenv=require('dotenv')
 const mongoose=require('mongoose')
 
-dotenv.config({path:"./config.env"})
+//dotenv.config({path:"./config.env"})
 
 const db=process.env.DATABASE.replace('<password>',process.env.DATABASE_PASSWORD)
 
